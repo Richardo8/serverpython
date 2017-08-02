42123e061f6754e8a382a5456da7e63328b378a7
LS <manunited_22@sina.com>
2017-08-02 17:06:11 +0800

    整理

--- userlist/views.py
+++ userlist/views.py
@@ -2,10 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from userlist.models import UserList
 from django.core import serializers
+import json
 
 def getAllUser(request):
     userlist = UserList.objects.all()
     returnData = serializers.serialize('json', userlist)
+    # listData = list(userlist)
+    # returnData = json.dumps(listData)
+    # print(returnData)
     return HttpResponse(returnData, content_type="application/json")
 
 
