5402d9251c580e70676824f244797ceb8ba632d2
LS <manunited_22@sina.com>
2017-08-04 11:46:17 +0800

    修改数据库数据类型

--- userlist/models.py
+++ userlist/models.py
@@ -3,14 +3,14 @@ from django.db import models
 
 class UserList(models.Model):
     uid = models.IntegerField()
-    nick = models.CharField(max_length=30)
-    name = models.CharField(max_length=30)
-    sex = models.CharField(max_length=30)
+    nick = models.TextField(max_length=30)
+    name = models.TextField(max_length=30)
+    sex = models.TextField(max_length=30)
     phone = models.CharField(max_length=30)
-    birthday = models.CharField(max_length=30)
-    address = models.CharField(max_length=30)
-    latest = models.CharField(max_length=30)
-    identity = models.CharField(max_length=30)
+    birthday = models.DateField(auto_now=False)
+    address = models.TextField(max_length=30)
+    latest = models.DateTimeField(auto_now=True)
+    identity = models.TextField(max_length=30)
 
     # def __str__(self):
     #     return ({
