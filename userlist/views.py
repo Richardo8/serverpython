6a29711e80080b631c890068bbf1983422534dba
LS <manunited_22@sina.com>
2017-08-04 10:26:38 +0800

    get方法只能获取一条数据

--- userlist/views.py
+++ userlist/views.py
@@ -33,12 +33,34 @@ def getAllUser(request):
 
 def search(request):
     searchBody = json.loads(request.body)
-    searchResult = UserList.objects.get(name=searchBody['name'])
-    # 先将querySet实例转化成dict
-    searchResult_dict = model_to_dict(searchResult)
-    # 然后将dict转化成json，最后返回
-    searchResult_return = json.dumps(searchResult_dict)
-    print(searchResult_return)
-    return HttpResponse(searchResult_return, content_type="application/json")
+    # # 该方法只能返回一条数据
+    # searchResult = UserList.objects.get(name=searchBody['name'])
+    # # 先将querySet实例转化成dict
+    # searchResult_dict = [model_to_dict(searchResult)]
+    # # 然后将dict转化成json，最后返回
+    # searchResult_return = json.dumps(searchResult_dict)
+    # print(searchResult_return)
+
+    searchMore = UserList.objects.filter(name=searchBody['name'])
+    print(searchMore)
+    returnList = []
+    for e in searchMore:
+        print(e.uid)
+        data = {
+            "uid": e.uid,
+            "nick": e.nick,
+            "name": e.name,
+            "sex": e.sex,
+            "phone": e.phone,
+            "birthday": e.birthday,
+            "address": e.address,
+            "latest": e.latest,
+            "identity": e.identity
+        }
+        returnList.append(data)
+    # 这种方式可以将数据提取出来，然后转化成json格式并传输
+    returnList_json = json.dumps(returnList)
+
+    return HttpResponse(returnList_json, content_type="application/json")
 
 # Create your views here.
