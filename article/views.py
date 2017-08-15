83815efa7bb3184656b3e7ca99011ca33813309e
LS <manunited_22@sina.com>
2017-08-15 17:38:06 +0800

    将图片上传到react目录下

--- article/views.py
+++ article/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 def uploadPic(request):
     file_obj = request.FILES.get('avatar')
     print(file_obj)
-    file_path = os.path.join('article/static/images', file_obj.name)
+    file_path = os.path.join('/Users/liushuo/Documents/Code/Study/React/reactirm/public/Pic', file_obj.name)
     with open(file_path, 'wb') as f:
         for chunk in file_obj.chunks():
             f.write(chunk)
