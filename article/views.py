6f5f5f3929304396fafc6b3425f9b426158ba385
LS <manunited_22@sina.com>
2017-08-18 11:45:24 +0800

    修改上传图片

--- article/views.py
+++ article/views.py
@@ -5,10 +5,12 @@ from django.http import HttpResponse
 def uploadPic(request):
     file_obj = request.FILES.get('avatar')
     print(file_obj)
-    file_path = os.path.join('/Users/liushuo/Documents/Code/Study/React/reactirm/public/Pic', file_obj.name)
+    # file_path = os.path.join('/Users/liushuo/Documents/Code/Study/React/reactirm/public/Pic', file_obj.name)
+    file_path = os.path.join('common_static/images', file_obj.name)
+    print(file_path)
     with open(file_path, 'wb') as f:
         for chunk in file_obj.chunks():
             f.write(chunk)
-    return HttpResponse(file_obj.name)
+    return HttpResponse('static/images/' + file_obj.name, content_type="application/json")
 
 # Create your views here.
